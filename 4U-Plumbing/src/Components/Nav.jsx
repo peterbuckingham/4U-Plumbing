929474a347a96d959bcac7cf76d936c6cb327f25
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-18 16:28:14 +0200

    Navbar almost done, topnav styles left to complete

--- 4U-Plumbing/src/Components/Nav.jsx
+++ 4U-Plumbing/src/Components/Nav.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import Logo from "../images/logo-transparent.png";
+import '../styles/index.css'
 
 function Nav() {
   return (
     <div>
-        
       <div className="top-nav">
-        <div className="4uPlumbingLogo"><img src="" alt="4u-Plumbing-Logo" /></div>
-        <span>
-          <p className="Email-us">E-mail us:</p>4U-Plumbing.co.za
+        <div className="topNavShape"></div>
+        <div>
+          <img className="fouruPlumbingLogo" src={Logo} alt="4U-PlumbingLogo" />
+          <p className="fouruPlumbingLogo-text">4U Plumbing</p>
+        </div>
+        <span className="top-nav-emailUs">
+          <p className="email-Us">E-mail us:</p><p>4U-Plumbing.co.za</p>
         </span>
         <span>
           <div>
@@ -15,7 +19,7 @@ function Nav() {
           </div>
           <p>Monday - Friday</p>
         </span>
-        <span>
+        <span className="top-nav-socialMedia">
           <p className="Follow-us">Follow us:</p>
           <img src="" alt="FacebookLogo" />
           <img src="" alt="instagramLogo" />
@@ -23,6 +27,15 @@ function Nav() {
         </span>
       </div>
 
+      <div className="main-Nav">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">FAQ</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
