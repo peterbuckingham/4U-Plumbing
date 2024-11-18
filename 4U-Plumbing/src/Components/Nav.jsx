dde64edebbd0ce57cc32c2fdd6cb5fb80d895e49
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-18 11:24:29 +0200

    Top Nav Complete, Styling left

--- 4U-Plumbing/src/Components/Nav.jsx
+++ 4U-Plumbing/src/Components/Nav.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Nav() {
   return (
     <div>
-      
+        
+      <div className="top-nav">
+        <div className="4uPlumbingLogo"><img src="" alt="4u-Plumbing-Logo" /></div>
+        <span>
+          <p className="Email-us">E-mail us:</p>4U-Plumbing.co.za
+        </span>
+        <span>
+          <div>
+            <img src="" alt="clockIcon" />
+          </div>
+          <p>Monday - Friday</p>
+        </span>
+        <span>
+          <p className="Follow-us">Follow us:</p>
+          <img src="" alt="FacebookLogo" />
+          <img src="" alt="instagramLogo" />
+          <img src="" alt="tiktokLogo" />
+        </span>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
