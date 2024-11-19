3fa2b33fbcb373296b7f479a673718744a76991c
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-19 10:43:13 +0200

    Top navbar design for laptop is done.

--- 4U-Plumbing/src/Components/Nav.jsx
+++ 4U-Plumbing/src/Components/Nav.jsx
@@ -1,6 +1,13 @@
-import Logo from "../images/logo-transparent.png"
-import '../styles/index.css'
+import React from "react";
+import Logo from "../images/logo-transparent.png";
+import "../styles/index.css";
 import { CiAlarmOn } from "react-icons/ci";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
+import {
+  faFacebook,
+  faInstagram,
+  faTiktok,
+} from "@fortawesome/free-brands-svg-icons"; // Import icons
 
 function Nav() {
   return (
@@ -12,29 +19,71 @@ function Nav() {
           <p className="fouruPlumbingLogo-text">4U Plumbing</p>
         </div>
         <span className="top-nav-emailUs">
-          <p className="email-Us">E-mail us:</p><p>4U-Plumbing.co.za</p>
+          <p className="email-Us">E-mail us:</p>
+          <p>4U-Plumbing.co.za</p>
         </span>
         <span className="top-nav-operatingTimes">
           <div>
-          <CiAlarmOn className="clockIcon" />
+            <CiAlarmOn className="clockIcon" />
           </div>
-          <p>Weekdays (Monday to Friday)</p>
+          <p className="topnav-times">Operating Times: </p><p>Weekdays (Monday to Friday)</p>
+          <p className="topnav-contact">Contact: </p><p>+27 82 590 7453</p>
         </span>
         <span className="top-nav-socialMedia">
           <p className="Follow-us">Follow us:</p>
-          <img src="" alt="FacebookLogo" />
-          <img src="" alt="instagramLogo" />
-          <img src="" alt="tiktokLogo" />
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faFacebook}
+              size="1x"
+              style={{ color: "#f7f9ff", marginRight: "20px" }}
+            />
+          </a>
+          <a
+            href="https://www.tiktok.com/@4u_plumbing?is_from_webapp=1&sender_device=pc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faTiktok}
+              size="1x"
+              style={{ color: "#f7f9ff", marginRight: "20px" }}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="1x"
+              style={{ color: "#f7f9ff" }}
+            />
+          </a>
         </span>
       </div>
 
       <div className="main-Nav">
         <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Services</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">FAQ</a></li>
-          <li><a href="">Contact</a></li>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
         </ul>
       </div>
     </div>
