a491a1dd85cb3b943536645b3fce37bd9c71bb31
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-19 11:39:41 +0200

    hero section mostly completed

--- 4U-Plumbing/src/App.jsx
+++ 4U-Plumbing/src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/index.css'
 import Nav from './components/Nav.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   return (
     <div>
-      <Nav/>
+      <Nav />
+      <Hero />
     </div>
   )
 }
--- 4U-Plumbing/src/Components/Hero.jsx
+++ 4U-Plumbing/src/Components/Hero.jsx
@@ -1,11 +1,15 @@
+import "../styles/index.css";
 
-
-function Hero() {
+const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Certified Plumbing Experts with 20+ Years <br />of Excellence</h1>
+        <p className="hero-subtitle">Your trusted partner for reliable, professional, and efficient plumbing solutions.</p>
+        <a href="#services" className="hero-btn">Get a Free Quote Today</a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
